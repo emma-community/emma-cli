cd195eef0ab0515842405d3d1ce5579b0a51d9a9
ekabardinsky <ekabardinsky@emma.ms>
2022-03-04 10:44:59 +0500

    Estore/K8s simple operations implementation

---
+++ src/commands/k8s/schema.ts
@@ -0,0 +1,29 @@
+import {Flags} from '@oclif/core'
+import constants from '../../utils/constants'
+import BaseCommand from '../../commons/base-command'
+
+export default class K8sSchema extends BaseCommand {
+  static description = 'Get application configuration schema'
+
+  static examples = [
+    `$ ${constants.cliName} k8s schema`,
+  ]
+
+  static flags = {
+    json: Flags.boolean({description: 'Print a json output', required: false}),
+    yaml: Flags.boolean({description: 'Print a yaml output', required: false}),
+    applicationId: Flags.integer({description: 'Application id', required: true}),
+  }
+
+  static args = [
+  ]
+
+  async run(): Promise<any> {
+    const {flags} = await this.parse(K8sSchema)
+    const params = this.getParamsMap(flags)
+    const publicApiService = this.getPublicApiService()
+
+    const response = await publicApiService.get(`k8s/schema/${params.applicationId}`, params)
+    this.printObject(flags, response.data)
+  }
+}
