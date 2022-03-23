611d93490ef4ace3bb0bedc8aaf83e342e0975dc
ekabardinsky <ekabardinsky@emma.ms>
2022-03-23 09:39:01 +0500

    Add custom help handler for not found commands

---
+++ src/commands/help.ts
@@ -0,0 +1,20 @@
+import BaseCommand from '../commons/base-command'
+import {Help} from '@oclif/core';
+
+export default class HelpWrapper extends BaseCommand {
+  static hidden = true
+
+  async run(): Promise<any> {
+    const topicName = this.argv[0]
+
+    // print command not found
+    this.printObject({}, 'The command is incomplete. Please check command usage:\n')
+
+    // if topic name provided
+    if (topicName) {
+      const help = new Help(this.config)
+      // @ts-ignore
+      await help.showTopicHelp({name: topicName})
+    }
+  }
+}
