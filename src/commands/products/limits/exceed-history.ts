79cd81eb8263db19c63f4e06d43f04cfc7062896
ekabardinsky <ekabardinsky@emma.ms>
2022-04-22 11:59:36 +0500

    Remove company common data readers in CLI

--- src/commands/products/limits/exceed-history.ts
+++ src/commands/products/limits/exceed-history.ts
@@ -2,14 +2,13 @@ import {Flags} from '@oclif/core'
 import BaseCommand from '../../../commons/base-command'
 
 export default class LimitsExceedHistory extends BaseCommand {
-  static description = 'Get current company project payable requests count'
+  static description = 'Get current project payable requests count'
 
   static flags = {
     json: Flags.boolean({description: 'Print a json output', required: false}),
     yaml: Flags.boolean({description: 'Print a yaml output', required: false}),
     startPeriodUnixTimestamp: Flags.integer({description: 'Start period for limits search (in unix timestamp)', required: true}),
     endPeriodUnixTimestamp: Flags.integer({description: 'End period for limits search (in unix timestamp)', required: true}),
-    companyLimits: Flags.boolean({description: 'Include company exceed history', required: false}),
   }
 
   static args = [
